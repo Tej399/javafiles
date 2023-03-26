7edf097d7caa06029998c437103d3df60d866985
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:16:26 +0530

    Create Main (21).java

---
+++ Main (21).java
@@ -0,0 +1,16 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+public static void main(String args[])
+{
+    char c=65;
+    char ch='A';
+System.out.println(c);
+System.out.println(ch);
+}
+}
