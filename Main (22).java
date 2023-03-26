2b4149df4c4bf4f8d5bafe9130b0193bef1af7a6
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:17:47 +0530

    Create Main (22).java

---
+++ Main (22).java
@@ -0,0 +1,21 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+
+{
+public static void main(String args[])
+{
+    byte b= 52;
+    short s=1508;
+    int i=548154661;
+    long l=616181855;
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+}
+}
