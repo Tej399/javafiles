4157ddfeb1d83df570836a705b77d09824e204bb
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:29:37 +0530

    Create Main (9).java

---
+++ Main (9).java
@@ -0,0 +1,14 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+public static void main(String args[])
+{
+    int a=10,b=40;
+System.out.println("Valu is "+a+b);
+}
+}
