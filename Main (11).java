7a092d887b1b672232937a1df14270afa59a899c
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:03:57 +0530

    Create Main (11).java

---
+++ Main (11).java
@@ -0,0 +1,16 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+public static void main(String args[])
+{
+System.out.println(1);
+System.out.println(123);
+System.out.println("ABC");
+System.out.println("#");
+}
+}
