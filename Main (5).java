636bf70c7a7ec14d8c3b6e070ad7836a34eb86b2
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:26:21 +0530

    Create Main (5).java

---
+++ Main (5).java
@@ -0,0 +1,19 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+public static void main(String args[])
+{
+    int i=200;
+    long l=i;
+    float f=l;
+System.out.println("int is "+i);
+System.out.println("long is "+l);
+System.out.println("float is "+f);
+}
+}
+
