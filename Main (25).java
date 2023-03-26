aed5ac773ad12a5e198da62c294597bdb2ee88a1
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:20:04 +0530

    Create Main (25).java

---
+++ Main (25).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String args[])
+{
+    double d=200.06;
+    long l=(long)d;
+    int i=(int)l;
+System.out.println("Double data type value is "+d);
+System.out.println("long data tyap value is "+l);
+System.out.println("int data type value is "+i);
+}
+}
