cda1942d1b5075a89481150ff258c807ef46820c
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:22:59 +0530

    Create Main (28).java

---
+++ Main (28).java
@@ -0,0 +1,15 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+
+
+class Division {
+	public static void main(String[] args) {
+		int a=50,b=10;
+		int res= a/b;
+		System.out.print("Division is "+res);
+	}
+}
