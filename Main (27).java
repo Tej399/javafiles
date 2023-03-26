75ccf96943a6a911f56b0dd000c42319ef8db050
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:22:02 +0530

    Create Main (27).java

---
+++ Main (27).java
@@ -0,0 +1,13 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+class Multiplication {
+	public static void main(String[] args) {
+		int a=50,b=48;
+		int res= a*b;
+		System.out.print("Multiplication is "+res);
+			}
+}
+ 
