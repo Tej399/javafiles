7490c802f49a9c14d68dd0c0e0ddc6b97d1232c1
Tej399 <122878735+Tej399@users.noreply.github.com>
2023-03-26 23:14:18 +0530

    Create Main (2).java

---
+++ Main (2).java
@@ -0,0 +1,14 @@
+/******************************************************************************
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=50,b=48;
+	    int res=a-b;
+		System.out.println("subtraction is"+res);
+	}
+}
